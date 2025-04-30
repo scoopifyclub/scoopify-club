fee1335fd6c4cc79655f34a7797ae144ee4c3d55
Scoopify Services <services@scoopify.club>
2025-04-29 19:18:29 -0600

    Update middleware configuration to handle Edge Runtime compatibility

--- src/middleware.js
+++ src/middleware.js
@@ -95,13 +95,7 @@ export async function middleware(request) {
 // Configure which paths the middleware should run on
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         * - public folder
-         */
-        '/((?!_next/static|_next/image|favicon.ico|public/).*)',
+        // Skip all internal paths (_next, api)
+        '/((?!_next|api|static|.*\\..*|favicon.ico).*)',
     ],
 };
